0a5745245cedfb961a113faf57fe016d3d3ab97a
Dzuy Tran <dzuyt@perfectomobile.com>
2019-01-20 18:58:03 -0500

    Update iarNativeBuild file to reflect the tool location

--- iarNativeBuild.bat
+++ iarNativeBuild.bat
@@ -1,5 +1,5 @@
 setlocal
-set IAR_ARM_BUILD="C:\IAR_Systems\EmbeddedWorkbench8.2\common\bin\"
+set IAR_ARM_BUILD="C:\IAR_Systems\ARM_EmbeddedWorkbench8.2\common\bin\"
 
 %IAR_ARM_BUILD%\IarBuild.exe BasicDebugging.ewp -build Debug -log all
 endlocal
