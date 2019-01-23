d71a21a3cd909cbdca521d083014f29d021a4c01
Dzuy Tran <dktran65@gmail.com>
2019-01-23 15:33:27 -0800

    Update iarSCAandLoad.bat file

--- iarSCAandLoad.bat
+++ iarSCAandLoad.bat
@@ -1,2 +1,3 @@
-kwbuildproject --url http://192.168.0.99:8080/IARDEMO -f -o kwtables kwinject.out --replace-path C:\ProjectSandbox\IAR_ARM_C=IAR_ARM_C
+set CUR_DIR=%cd%
+kwbuildproject --url http://192.168.0.99:8080/IARDEMO -f -o kwtables kwinject.out --replace-path %CUR_DIR%\IAR_ARM_C=IAR_ARM_C
 kwadmin --url http://192.168.0.99:8080 load IARDEMO kwtables
