a30c0dbc8ad8d57c7c728cfc85212f60aa57afb9
Dzuy Tran <dktran65@gmail.com>
2019-01-23 14:34:12 -0800

    Update Batch Files

--- iarNativeBuild.bat
+++ iarNativeBuild.bat
@@ -1,5 +1,3 @@
 setlocal
-set IAR_ARM_BUILD="C:\IAR_Systems\ARM_EmbeddedWorkbench8.2\common\bin\"
-
-%IAR_ARM_BUILD%\IarBuild.exe BasicDebugging.ewp -build Debug -log all
+IarBuild.exe BasicDebugging.ewp -build Debug -log all
 endlocal
